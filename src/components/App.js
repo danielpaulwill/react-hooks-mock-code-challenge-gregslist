c15dea1fa15d7ece071338cd5876249e337244e7
Daniel Williams <danielpaulwill@gmail.com>
2022-01-15 14:52:45 -0500

    commit tree 166db384f48b886834ff8d88fdb53bef0fdac4e0

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+
+  console.log("listings:", listings)
+
+  function listingFetch() {
+    fetch('http://localhost:6001/listings')
+    .then(res => res.json())
+    .then(data => setListings(data))
+  }
+
+  useEffect(() => {
+    listingFetch()
+  }, [])
+
+  function handleOnDeleteClick(id) {
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: 'DELETE',
+})
+    listingFetch()
+  }
+
   return (
     <div className="app">
       <Header />
-      <ListingsContainer />
+      <ListingsContainer listings={listings} onDeleteClick={handleOnDeleteClick} />
     </div>
   );
 }
 
 export default App;
+
+
+
+// ,
+//     {
+//       "id": 8,
+//       "description": "Test Listing",
+//       "image": "./images/toyota-grill.jpg",
+//       "location": "Test"
+//     }
